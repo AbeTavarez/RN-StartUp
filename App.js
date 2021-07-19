0fcb4f5c3c42549729643986b2c85a118b3e2cc5
Abraham E. Tavarez <abetavarez@icloud.com>
2021-07-19 13:25:38 -0400

    list screen

--- App.js
+++ App.js
@@ -2,16 +2,18 @@ import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
 import ComponentsScreen from "./src/screens/ComponentsScreen";
+import ListScreen from "./src/screens/ListScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
-    Componentns: ComponentsScreen
+    Componentns: ComponentsScreen,
+    List: ListScreen
   },
   {
-    initialRouteName: "Componentns",
+    initialRouteName: "List",
     defaultNavigationOptions: {
-      title: "App",
+      title: "Agile Linux",
     },
   }
 );
